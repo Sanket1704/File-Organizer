5210827abbab95bea26dcc46f743a6dd819b7b36
Sanket1704 <92672728+Sanket1704@users.noreply.github.com>
2023-09-07 17:18:55 +0530

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,10 @@
+function help(){
+    console.log(`List of all commands is:-
+    node main.js tree "directoryPath"
+    node main.js organize "directoryPath"
+    mode main.js help
+   `)
+}
+module.exports = {
+    helpKey : help
+}
